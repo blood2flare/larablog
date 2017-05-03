4f873dfd5b16f3aec1bffe8562bf4284b37b6da1
Brad <blood2flare@yahoo.com>
2017-05-03 16:26:26 +0700

    Step 16 done

--- Project/app/Http/Controllers/PostController.php
+++ Project/app/Http/Controllers/PostController.php
@@ -15,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view('posts.index')->with('posts', $posts);
     }
 
     /**
@@ -62,7 +63,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::find($id);
+        return view('posts.show')->with('post', $post);
     }
 
     /**
--- Project/resources/views/posts/show.blade.php
+++ Project/resources/views/posts/show.blade.php
@@ -6,7 +6,33 @@
 @endsection
 
 @section('content')
-  <p class="lead">This is the blog post</p>
+  <div class="row">
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
+      <p class="lead">{{ $post->body }}</p>
+    </div>
+    <div class="col-md-4">
+      <div class="well">
+        <dl class="dl-horizontal">
+          <dt>Created At: Date</dt>
+          <dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+        </dl>
+        <dl class="dl-horizontal">
+          <dt>Last Updated: Date</dt>
+          <dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+        </dl>
+        <hr>
+        <div class="row">
+          <div class="col-sm-6">
+            {!! Html::linkRoute('posts.edit', 'Edit', [ $post->id ] , ['class' => 'btn btn-primary btn-block']) !!}
+          </div>
+          <div class="col-sm-6">
+            {!! Html::linkRoute('posts.destroy', 'Delete', [ $post->id ] , ['class' => 'btn btn-danger btn-block']) !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @section('scripts')
